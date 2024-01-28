811cf838edcb830fff390fbda1cb30f8b9f3d398
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2024-01-28 13:49:09 +1300

    Update Admin.ps1

--- Admin.ps1
+++ Admin.ps1
@@ -51,6 +51,7 @@ Set-Service -Name WSearch -StartupType Disabled
 # Disable Windows Web Search
 Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" -Name "BingSearchEnabled" -Value 0
 
-# Re-Enable Full Right-Click Context Menus
+# Restore Full Right-Click Menu
 New-Item -Path "HKCU:\Software\Classes\CLSID" -Name "{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}"
 New-Item -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" -Name InprocServer32
+Set-ItemProperty -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" -Name "(Default)" -Value 0
