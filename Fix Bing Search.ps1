a075fd20a08cc749ea6599ae841e39865b79f3d8
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2024-08-28 12:43:00 +1200

    additions to the disable bing script

--- Fix Bing Search.ps1
+++ Fix Bing Search.ps1
@@ -1,9 +1,11 @@
 if (Test-Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" -ErrorAction SilentlyContinue) {
     $val = Get-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" -Name "BingSearchEnabled" -ErrorAction SilentlyContinue
-    if ($val.BingSearchEnabled -eq 0) {
+    if ($val -and $val.BingSearchEnabled -eq 0) {
         Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" -Name "BingSearchEnabled" -Value 1
     }
-    elseif ($val.BingSearchEnabled -eq 1) {
+    elseif ($val -and $val.BingSearchEnabled -eq 1) {
+        New-Item -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" -Force
+        New-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" -Name "BingSearchEnabled" -Value 0 -Force
         Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" -Name "BingSearchEnabled" -Value 0
     }
 }
