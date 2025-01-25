63dcfec44a0c4e5302957431d0cb9ca3d87df1db
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2025-01-25 15:16:23 +1300

    suggestions and more

--- New PC Cleanup/RunAsAdmin.ps1
+++ New PC Cleanup/RunAsAdmin.ps1
@@ -17,3 +17,19 @@ Remove-Item -Path "Registry::HKEY_CLASSES_ROOT\Directory\shellex\ContextMenuHand
 Remove-Item -Path "Registry::HKEY_CLASSES_ROOT\Folder\ShellEx\ContextMenuHandlers\pintostart"
 # Remove Pin to Home - Folders
 Remove-Item -Path "Registry::HKEY_CLASSES_ROOT\Folder\shell\pintohome"
+
+# Pin to Start
+Remove-Item -Path "Registry::HKEY_CLASSES_ROOT\*\shellex\ContextMenuHandlers\{a2a9545d-a0c2-42b4-9708-a0b2badd77c8}" -Force
+
+# Remove AMD
+New-Item -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Shell Extensions\Blocked"
+New-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Shell Extensions\Blocked" -Name "{6767B3BC-8FF7-11EC-B909-0242AC120002}" -Value 'AMDSoftwareAdrenalinEdition' -PropertyType String -Force
+
+# Remove Restore Previous Versions
+New-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Shell Extensions\Blocked" -Name "{596AB062-B4D2-4215-9F74-E9109B0A8153}" -Value '' -PropertyType String -Force
+
+# Potential Locations
+# HKEY_CLASSES_ROOT\AllFilesystemObjects\shellex\ContextMenuHandlers\
+# HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Shell Extensions\Approved
+# Computer\HKEY_LOCAL_MACHINE\SOFTWARE\Classes\Directory\shellex
+# Also try: Easy Context Menu software
