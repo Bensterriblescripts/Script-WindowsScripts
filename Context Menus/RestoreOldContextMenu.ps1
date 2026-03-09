3425c2050d9ef8ae91784853fabf3979f0e1d77d
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2026-03-10 10:50:19 +1300

    added an explorer restart to the contextmenu restore

--- Context Menus/RestoreOldContextMenu.ps1
+++ Context Menus/RestoreOldContextMenu.ps1
@@ -1,4 +1,10 @@
-# Restore Full Right-Click Menu
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+  Start-Process powershell.exe -Verb RunAs -ArgumentList '-NoProfile','-ExecutionPolicy','Bypass','-File',('"{0}"'-f$PSCommandPath); exit
+}
+
 New-Item -Path "HKCU:\Software\Classes\CLSID" -Name "{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}"
 New-Item -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" -Name InprocServer32
 Set-ItemProperty -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" -Name "(Default)" -Value 0
+
+Stop-Process -Name explorer -Force
+Start-Process explorer
