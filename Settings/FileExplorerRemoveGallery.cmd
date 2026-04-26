4b30ced9545874ca0435ad6e2cbf9d6a934f6670
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2026-04-26 16:24:35 -0700

    fixed the cmd escalation error, added git context menu

--- Settings/FileExplorerRemoveGallery.cmd
+++ Settings/FileExplorerRemoveGallery.cmd
@@ -1,5 +1,8 @@
-if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-  Start-Process powershell.exe -Verb RunAs -ArgumentList '-NoProfile','-ExecutionPolicy','Bypass','-File',('"{0}"'-f$PSCommandPath); exit
-}
+@echo off
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    mshta "vbscript:CreateObject(""Shell.Application"").ShellExecute(""cmd.exe"", ""/c """"%~f0"""" "", """", ""runas"", 1)(window.close)"
+    exit /b
+)
 
 reg delete HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace\{e88865ea-0e1c-4e20-9aa6-edcd0212c87c} /f
