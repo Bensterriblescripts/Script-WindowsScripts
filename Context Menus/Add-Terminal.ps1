fbc05be8f17e51d5e0738231456fc59617c50456
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2026-05-22 22:10:52 +1200

    newline bad

--- Context Menus/Add-Terminal.ps1
+++ Context Menus/Add-Terminal.ps1
@@ -1,13 +1,5 @@
 # In Background of Folder
-New-Item -Path 'HKCU:\Software\Classes\Directory\Background\shell\WindowsTerminal\command' -Force | Out-Null; Set-ItemProperty -Path
-'HKCU:\Software\Classes\Directory\Background\shell\WindowsTerminal' -Name '(default)' -Value 'Open in Terminal'; Set-ItemProperty -Path
-'HKCU:\Software\Classes\Directory\Background\shell\WindowsTerminal' -Name 'Icon' -Value (Get-Command wt.exe | Select-Object
--ExpandProperty Source); Set-ItemProperty -Path 'HKCU:\Software\Classes\Directory\Background\shell\WindowsTerminal\command' -Name
-'(default)' -Value ('"' + "$env:LOCALAPPDATA\Microsoft\WindowsApps\wt.exe" + '" -d "%V"')
+New-Item -Path 'HKCU:\Software\Classes\Directory\Background\shell\WindowsTerminal\command' -Force | Out-Null; Set-ItemProperty -Path 'HKCU:\Software\Classes\Directory\Background\shell\WindowsTerminal' -Name '(default)' -Value 'Open in Terminal'; Set-ItemProperty -Path 'HKCU:\Software\Classes\Directory\Background\shell\WindowsTerminal' -Name 'Icon' -Value (Get-Command wt.exe | Select-Object -ExpandProperty Source); Set-ItemProperty -Path 'HKCU:\Software\Classes\Directory\Background\shell\WindowsTerminal\command' -Name '(default)' -Value ('"' + "$env:LOCALAPPDATA\Microsoft\WindowsApps\wt.exe" + '" -d "%V"')
 
 # When Selecting Folder
-New-Item -Path 'HKCU:\Software\Classes\Directory\shell\WindowsTerminal\command' -Force | Out-Null; Set-ItemProperty -Path 'HKCU:
-\Software\Classes\Directory\shell\WindowsTerminal' -Name '(default)' -Value 'Open in Terminal'; Set-ItemProperty -Path 'HKCU:
-\Software\Classes\Directory\shell\WindowsTerminal' -Name 'Icon' -Value (Get-Command wt.exe | Select-Object -ExpandProperty Source); Set-
-ItemProperty -Path 'HKCU:\Software\Classes\Directory\shell\WindowsTerminal\command' -Name '(default)' -Value ('"' +
-"$env:LOCALAPPDATA\Microsoft\WindowsApps\wt.exe" + '" -d "%V"')
+New-Item -Path 'HKCU:\Software\Classes\Directory\shell\WindowsTerminal\command' -Force | Out-Null; Set-ItemProperty -Path 'HKCU:\Software\Classes\Directory\shell\WindowsTerminal' -Name '(default)' -Value 'Open in Terminal'; Set-ItemProperty -Path 'HKCU:\Software\Classes\Directory\shell\WindowsTerminal' -Name 'Icon' -Value (Get-Command wt.exe | Select-Object -ExpandProperty Source); Set-ItemProperty -Path 'HKCU:\Software\Classes\Directory\shell\WindowsTerminal\command' -Name '(default)' -Value ('"' +"$env:LOCALAPPDATA\Microsoft\WindowsApps\wt.exe" + '" -d "%V"')
