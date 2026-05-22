e28fbec82613bab17285a9bf1a66890045e9bf0a
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2026-05-22 22:07:48 +1200

    software chantges

--- Settings/Search-DisableBingSearch.ps1
+++ Settings/Search-DisableBingSearch.ps1
@@ -1,2 +1,4 @@
 # Disable Windows Web Search
 Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" -Name "BingSearchEnabled" -Value 0
+# Disable Store Search - Didn't work
+#Set-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows\Explorer" -Name "DisableSearchBoxSuggestions" -Value 1
