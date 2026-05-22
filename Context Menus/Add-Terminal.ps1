b03e9826bc34eef51a88caeacafc56fb319db10c
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2026-05-23 08:18:58 +1200

    added icon restore

--- Context Menus/Add-Terminal.ps1
+++ Context Menus/Add-Terminal.ps1
@@ -1,5 +1,34 @@
-# In Background of Folder
-New-Item -Path 'HKCU:\Software\Classes\Directory\Background\shell\WindowsTerminal\command' -Force | Out-Null; Set-ItemProperty -Path 'HKCU:\Software\Classes\Directory\Background\shell\WindowsTerminal' -Name '(default)' -Value 'Open in Terminal'; Set-ItemProperty -Path 'HKCU:\Software\Classes\Directory\Background\shell\WindowsTerminal' -Name 'Icon' -Value (Get-Command wt.exe | Select-Object -ExpandProperty Source); Set-ItemProperty -Path 'HKCU:\Software\Classes\Directory\Background\shell\WindowsTerminal\command' -Name '(default)' -Value ('"' + "$env:LOCALAPPDATA\Microsoft\WindowsApps\wt.exe" + '" -d "%V"')
+$WtCommand = Get-Command wt.exe -ErrorAction SilentlyContinue
+$WtLauncher = "$env:LOCALAPPDATA\Microsoft\WindowsApps\wt.exe"
+if (-not (Test-Path $WtLauncher) -and $WtCommand -and (Test-Path $WtCommand.Source)) {
+  $WtLauncher = $WtCommand.Source
+}
 
-# When Selecting Folder
-New-Item -Path 'HKCU:\Software\Classes\Directory\shell\WindowsTerminal\command' -Force | Out-Null; Set-ItemProperty -Path 'HKCU:\Software\Classes\Directory\shell\WindowsTerminal' -Name '(default)' -Value 'Open in Terminal'; Set-ItemProperty -Path 'HKCU:\Software\Classes\Directory\shell\WindowsTerminal' -Name 'Icon' -Value (Get-Command wt.exe | Select-Object -ExpandProperty Source); Set-ItemProperty -Path 'HKCU:\Software\Classes\Directory\shell\WindowsTerminal\command' -Name '(default)' -Value ('"' +"$env:LOCALAPPDATA\Microsoft\WindowsApps\wt.exe" + '" -d "%V"')
+$WtIcon = $null
+$WtPackage = Get-AppxPackage Microsoft.WindowsTerminal -ErrorAction SilentlyContinue
+if ($WtPackage) {
+  $WtIcon = @(
+    (Join-Path $WtPackage.InstallLocation 'Images\terminal_contrast-black.ico')
+    (Join-Path $WtPackage.InstallLocation 'Images\terminal_contrast-white.ico')
+    (Join-Path $WtPackage.InstallLocation 'WindowsTerminal.exe')
+    (Join-Path $WtPackage.InstallLocation 'wt.exe')
+  ) | Where-Object { Test-Path $_ } | Select-Object -First 1
+}
+
+if (-not $WtIcon -and $WtCommand -and (Test-Path $WtCommand.Source)) {
+  $WtIcon = $WtCommand.Source
+}
+
+if (-not $WtIcon) {
+  $WtIcon = "$env:SystemRoot\System32\shell32.dll,0"
+}
+
+foreach ($BaseKey in @(
+  'HKCU:\Software\Classes\Directory\Background\shell\WindowsTerminal'
+  'HKCU:\Software\Classes\Directory\shell\WindowsTerminal'
+)) {
+  New-Item -Path "$BaseKey\command" -Force | Out-Null
+  Set-ItemProperty -Path $BaseKey -Name '(default)' -Value 'Open in Terminal'
+  Set-ItemProperty -Path $BaseKey -Name 'Icon' -Value $WtIcon
+  Set-ItemProperty -Path "$BaseKey\command" -Name '(default)' -Value ('"' + $WtLauncher + '" -d "%V"')
+}
